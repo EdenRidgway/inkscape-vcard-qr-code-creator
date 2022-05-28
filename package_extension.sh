ccae47ca80595433450d280c88d55f8f08428d10
Moini <moini@noreply.invalid>
2022-05-28 04:08:10 +0200

    Ignore Python cache files for packaging.

--- package_extension.sh
+++ package_extension.sh
@@ -3,7 +3,7 @@
 today=$(date +%Y.%m.%d.%H.%M)
 zipfilename="/tmp/vcardqr_$today.zip"
 
-zip -r $zipfilename ./* -x .git* -x pylint* -x .pep8 -x Makefile -x "no-package*" -x package_extension.sh
+zip -r $zipfilename ./* -x .git* -x pylint* -x .pep8 -x Makefile -x "no-package*" -x "*__pycache__*" -x package_extension.sh
 
 gpg --detach-sign --armor -u maren@goos-habermann.de -o $zipfilename.sig $zipfilename
 
